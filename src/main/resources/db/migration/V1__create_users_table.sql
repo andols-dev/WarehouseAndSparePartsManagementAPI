b32679730dee9bd205b9a9a9a50d7fc3d97eb3e0
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-08-30 19:48:51 +0200

    create user table

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+   id BIGINT IDENTITY(1,1) PRIMARY KEY,
+   username VARCHAR(50) NOT NULL UNIQUE,
+   first_name VARCHAR(100) NOT NULL,
+   last_name VARCHAR(100) NOT NULL,
+   email VARCHAR(255) NOT NULL UNIQUE,
+   password VARCHAR(255) NOT NULL,
+   active BIT NOT NULL DEFAULT 1
+);
