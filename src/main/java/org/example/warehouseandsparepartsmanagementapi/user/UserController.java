81099eb2bd0b3e6b5827c914d4680cdf086a69ce
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-08-31 16:43:20 +0200

    create usercontroller and userservice

---
+++ src/main/java/org/example/warehouseandsparepartsmanagementapi/user/UserController.java
@@ -0,0 +1,34 @@
+package org.example.warehouseandsparepartsmanagementapi.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        Optional<User> user = userService.getUserById(id);
+        if(user.isPresent()){
+            return ResponseEntity.ok(user.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ src/main/java/org/example/warehouseandsparepartsmanagementapi/user/UserService.java
@@ -0,0 +1,25 @@
+package org.example.warehouseandsparepartsmanagementapi.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService
+{
+    private UserRepository userRepository;
+    public UserService(UserRepository userRepository)
+    {}
+
+    public List<User> getUsers()
+    {
+        return userRepository.findAll();
+    }
+
+
+    public Optional<User> getUserById(Long id)
+    {
+        return userRepository.findById(id);
+    }
+}
