d095b33e334200ca735db2ab9493f48d746e0179
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-09-03 18:53:26 +0200

    configure openapi and swagger

---
+++ src/main/java/org/example/warehouseandsparepartsmanagementapi/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package org.example.warehouseandsparepartsmanagementapi.config;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI warehouseApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Warehouse and Spare Parts Management API")
+                        .version("1.0.0")
+                        .description("REST API for warehouse and spare parts management"))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
