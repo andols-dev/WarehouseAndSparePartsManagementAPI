04514bf8ead68ea3e44bbd61434445f3b3f85059
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-09-03 09:21:32 +0200

    create jwtconfig

--- src/main/java/org/example/warehouseandsparepartsmanagementapi/security/SecurityConfig.java
+++ src/main/java/org/example/warehouseandsparepartsmanagementapi/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,7 +26,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+
         return http.build();
     }
     @Bean
